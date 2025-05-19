74e96801080371588ab2f3c2e7334df3b068036d
Oladapo Adeleke <oladapo_joseph@yahoo.com>
2025-05-20 00:31:18 +0100

    question 3

--- Assessment_Q2.sql
+++ Assessment_Q2.sql
@@ -1,13 +1,21 @@
 USE adashi_staging;
 
-WITH monthlytransactions as (SELECT owner_id, count(owner_id) as frequency, date_format(transaction_date, '%Y-%m') yearmonth ,
-								CASE WHEN count(owner_id) <=2 THEN "Low Frequency" 
+-- this creates a temporary table to get customer frequency of transactions 
+WITH monthlytransactions as (
+                            SELECT owner_id, 
+                                    count(owner_id) as frequency, 
+                                    date_format(transaction_date, '%Y-%m') yearmonth ,
+								CASE 
+                                    WHEN count(owner_id) <=2 THEN "Low Frequency" 
 									WHEN count(owner_id) <9 THEN "Medium Frequency"
                                     ELSE 'High Frequency'
                                     END as frequency_category
 							FROM savings_savingsaccount
-							GROUP BY date_format(transaction_date, '%Y-%m'), owner_id) 
+							GROUP BY date_format(transaction_date, '%Y-%m'), owner_id
+                            ) 
                             
-SELECT frequency_category, count(frequency_category), avg(frequency)
+SELECT frequency_category, 
+        count(frequency_category), 
+        avg(frequency)
 FROM monthlytransactions
 GROUP BY frequency_category
---
+++ Assessment_Q3.sql
@@ -0,0 +1,34 @@
+-- Switch to the adashi_staging database
+USE adashi_staging;
+
+-- CTE to find inactive savings and investment plans (no activity for over 365 days)
+WITH savings_inactive AS (
+    -- Get last transaction date for savings accounts with non-zero amount
+    SELECT 
+        plan_id, 
+        owner_id, 
+        'savings', 
+        MAX(transaction_date) AS Last_transaction_date, 
+        DATEDIFF(CURDATE(), MAX(transaction_date)) AS inactivity_days
+    FROM savings_savingsaccount
+    WHERE amount != 0
+    GROUP BY plan_id, owner_id
+    HAVING inactivity_days > 365
+
+    UNION ALL
+
+    -- Get last start date for investment plans with non-zero amount
+    SELECT 
+        plan_type_id, 
+        owner_id, 
+        'investment', 
+        MAX(start_date) AS Last_transaction_date, 
+        DATEDIFF(CURDATE(), MAX(start_date)) AS inactivity_days
+    FROM plans_plan
+    WHERE amount != 0
+    GROUP BY plan_type_id, owner_id
+    HAVING inactivity_days > 365
+)
+
+-- Return all inactive savings and investment plans
+SELECT * FROM savings_inactive;
